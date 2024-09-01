8678b7df1c0ab0e708c1910bb91e9af76ca43294
Michael Robins <michaelarobins93@gmail.com>
2024-08-31 21:45:40 -0600

    small commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
-
+import Header from "@/app/components/Header";
 export default function Main() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <Header />
+      <section className="flex flex-col items-center justify-center space-y-4">
+        <h1 className="text-4xl font-bold">Welcome to NexMail</h1>
+        <p className="text-lg">A simple email client built with Next.js</p>
+        {/* <Image src="/mail.svg" width={200} height={200} alt="Mailbox" /> */}
+      </section>
     </main>
   );
 }
